e1fd5b9e16d4d31ab5e3d46d0eed1d7432393777
Ciaran McCormick <ciaran@grokrconsulting.com>
2020-01-20 11:29:11 +0000

    Added viewsets for category, exercises and sets

--- django_fitnotes/exercises/views.py
+++ django_fitnotes/exercises/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from exercises.models import Category, Exercise, Set
+from exercises.pagination import LimitOffset100Pagination
+from exercises.serializers import CategorySerializer, ExerciseSerializer, SetSerializer
+
+
+class CategoryReadOnlyViewSet(ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = LimitOffset100Pagination
+
+
+class ExerciseReadOnlyViewSet(ReadOnlyModelViewSet):
+    queryset = Exercise.objects.all()
+    serializer_class = ExerciseSerializer
+    pagination_class = LimitOffset100Pagination
+
+
+class SetReadOnlyViewSet(ReadOnlyModelViewSet):
+    queryset = Set.objects.all()
+    serializer_class = SetSerializer
+    pagination_class = LimitOffset100Pagination
