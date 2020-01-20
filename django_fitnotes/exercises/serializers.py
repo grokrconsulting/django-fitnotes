54fbd58678b19c5deb4cb3534c148038e0f08b67
Ciaran McCormick <ciaran@grokrconsulting.com>
2020-01-20 11:30:30 +0000

    Added serializers
    Added ModelSerializers for Category, Exercise and Sets models.

--- django_fitnotes/exercises/serializers.py
+++ django_fitnotes/exercises/serializers.py
@@ -6,13 +6,16 @@ from exercises.models import Category, Exercise, Set
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
+        fields = "__all__"
 
 
 class ExerciseSerializer(ModelSerializer):
     class Meta:
         model = Exercise
+        fields = "__all__"
 
 
 class SetSerializer(ModelSerializer):
     class Meta:
         model = Set
+        fields = "__all__"
